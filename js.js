cedf634faf513877ec77c284273148eb7664cbc5
Tsuchiya-Ryo <66044977+Tsuchiya-Ryo@users.noreply.github.com>
2020-09-11 13:45:35 +0900

    test

---
+++ js.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("input[type=number]").on("focus", function() {
+        $(this).on("keydown", function(event) {
+            if (event.keyCode === 38 || event.keyCode === 40) {
+                event.preventDefault();
+            }
+        });
+    });
+
+});
